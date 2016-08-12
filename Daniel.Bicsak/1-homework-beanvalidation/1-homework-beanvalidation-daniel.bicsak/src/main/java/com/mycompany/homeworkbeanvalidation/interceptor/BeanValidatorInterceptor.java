6fc09b3079d59b895581ebc1d86525336f029e25
Bicsák Dániel <bicsak.daniel@gmail.com>
2016-08-12 13:10:47 +0200

    Fixed: enterMethod not catch exception

--- Daniel.Bicsak/1-homework-beanvalidation/1-homework-beanvalidation-daniel.bicsak/src/main/java/com/mycompany/homeworkbeanvalidation/interceptor/BeanValidatorInterceptor.java
+++ Daniel.Bicsak/1-homework-beanvalidation/1-homework-beanvalidation-daniel.bicsak/src/main/java/com/mycompany/homeworkbeanvalidation/interceptor/BeanValidatorInterceptor.java
@@ -5,8 +5,6 @@ import com.mycompany.homeworkbeanvalidation.annotations.ValidatorInterceptor;
 import com.mycompany.homeworkbeanvalidation.exceptions.validator.ValidationException;
 import java.util.Optional;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.inject.Inject;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
@@ -21,35 +19,30 @@ import javax.validation.Validator;
 @Interceptor
 @ValidatorInterceptor
 public class BeanValidatorInterceptor {
-    
+
     @Inject
     private Validator validator;
-    
+
     @AroundInvoke
     public Object enterMethod(InvocationContext ic) throws Exception {
         validateParameters(ic.getParameters());
-        try {
-            return ic.proceed();
-        } catch (Exception ex) {
-            Logger.getLogger(BeanValidatorInterceptor.class.getName()).log(Level.SEVERE, null, ex);
-            throw ex;
-        }
+        return ic.proceed();
     }
 
     private void validateParameters(Object[] parameters) {
         for (Object parameter : parameters) {
             Class<?> clazz = parameter.getClass();
-            if(clazz.isAnnotationPresent(ValidateBean.class)){
+            if (clazz.isAnnotationPresent(ValidateBean.class)) {
                 validatingBean(parameter);
             }
         }
     }
 
-    private void validatingBean(Object o){
+    private void validatingBean(Object o) {
         Set<ConstraintViolation<Object>> violations = validator.validate(o);
-        Optional<String> errorMessage = violations.stream().map(e -> "Validation error: " + e.getMessage()  + " - property: " + e.getPropertyPath().toString() + " . ").reduce(String::concat);
+        Optional<String> errorMessage = violations.stream().map(e -> "Validation error: " + e.getMessage() + " - property: " + e.getPropertyPath().toString() + " . ").reduce(String::concat);
         if (errorMessage.isPresent()) {
-            throw new ValidationException("Error during bean validation: "+violations.iterator().next().getMessage());
+            throw new ValidationException("Error during bean validation: " + violations.iterator().next().getMessage());
         }
     }
 
